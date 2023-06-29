1a4ecb464ae15d62613bf8ba24965053cecf325a
beeth0ven <beeth0vendev@gmail.com>
2023-06-29 09:14:37 +0800

    refactor - export package `provider`

--- lib/flutter_love_provider.dart
+++ lib/flutter_love_provider.dart
@@ -1,5 +1,6 @@
 library flutter_love_provider;
 
 export 'package:flutter_love/flutter_love.dart';
+export 'package:provider/provider.dart';
 export 'src/dispatch_x.dart' show DispatchX;
 export 'src/system_providers.dart' show SystemProviders;
