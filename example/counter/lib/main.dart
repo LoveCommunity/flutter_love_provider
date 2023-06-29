0892a9358289f5c49e28f155f48b62abfb005c66
beeth0ven <beeth0vendev@gmail.com>
2023-06-29 08:39:57 +0800

    fix lint warnings

--- example/counter/lib/main.dart
+++ example/counter/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_love_provider/flutter_love_provider.dart';
-import 'package:provider/provider.dart';
 
 // typedef CounterState = int;
 
